b757c102841ac1ce90b91101dd2bb447be3a4d55
Sean Kelly <shonkelli@gmail.com>
2017-11-22 22:55:12 -0500

    finished categories and fixed issue with products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,7 +11,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
-    @products = Product.all
+    @products = Product.where(["category_id = :category_id", {category_id: @category.id}])
   end
 
   # GET /categories/new
